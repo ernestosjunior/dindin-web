a7a133168de4f098995a6ef08fe3dcbb18461507
Ernesto Junior <ernesto.sjunior@hotmail.com>
2023-01-07 13:16:57 -0300

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Lato } from '@next/font/google'
+
+Lato({ weight: ['400', '700'], subsets: ['latin'], variable: '--lato-font' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
